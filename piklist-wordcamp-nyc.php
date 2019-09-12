f3195b903e3e090479fac51e0d7fef7e7b1ea360
Kevin Miller <kevin@p51labs.com>
2019-09-12 12:57:45 -0700

    Adding twitter validation rule

--- piklist-wordcamp-nyc.php
+++ piklist-wordcamp-nyc.php
@@ -145,13 +145,13 @@ function piklist_wcnyc_validate_twitter($index, $value, $options, $field, $field
   
   $response = wp_remote_get('https://twitter.com/users/username_available?username=' . $value);
   
-  if (is_array($response)) 
+  if (!is_wp_error($response)) 
   {
     $body = json_decode($response['body']);
     
     if (in_array($body->reason, array('available', 'invalid_username')))
     {
-      $valid = _('C`mon, we really want to follow you but we can`t follow a ghost!', 'piklist_wcnyc');
+      $valid = __('C`mon, we really want to follow you but we can`t follow a ghost!', 'piklist_wcnyc');
     }
   }
 
