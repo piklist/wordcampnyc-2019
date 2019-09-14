08a725c9564d176f3c3b1b3a9e098fda2edcba34
Steven Bruner <sbruner@slipfire.com>
2019-09-13 21:33:31 -0400

    admin notice

---
+++ parts/notices/lead-notice.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Notice Type: info
+Capability: manage_options
+Dismiss: true
+Page: post-new.php
+Post Type: Lead
+*/
+?>
+
+  <p>
+    <?php _e('Please enter real lead data only. No placeholder leads.', 'wordcampnyc-2019'); ?>
+  </p>
